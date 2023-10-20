b009d760769a35a90e1f25035b0b9795b14c50e8
shabnaz21 <shabnaz608@gmail.com>
2023-10-20 20:23:51 +0600

    product cart edit

--- src/Components/Pages/Error/Error.jsx
+++ src/Components/Pages/Error/Error.jsx
@@ -1,11 +1,19 @@
+import { BsArrowLeft } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Error = () => {
     return (
-        <div className="bg-[#ECECEC]">
-            <div className="mx-auto container md:pt-36">
-                <img src="https://i.ibb.co/L5jPHmZ/SVG-Animation-404-Page.gif" className="min-h-screen" alt="" />
+        <div>
+            <Link to='/' >
+                <div className="flex items-center gap-3 mx-48 mt-10">
+                    <p><BsArrowLeft></BsArrowLeft></p>
+                    <h2 className="text-2xl font-Rancho">Back to home </h2>
+                </div>
+            </Link>
+            <div className="flex place-content-center">
+                <img src="https://i.ibb.co/PYtJkQN/404.gif" alt="" />
             </div>
-      </div>
+        </div>
     );
 };
 
--- src/Components/Pages/Shopping/ProductCard.jsx
+++ src/Components/Pages/Shopping/ProductCard.jsx
@@ -41,12 +41,11 @@ const ProductCard = ({ product, products, setProducts }) => {
                 <div className="card md:card-side bg-base-100 shadow-xl">
                     <figure><img src={photo} alt={name} className="md:w-96 w-2/3 md:h-60 lg:h-[350px]"/></figure>
                     <div className="card-body">
-                        <div>
+                        <div className="space-y-2">
                             <h2 className="text-xl"><span className="font-bold">Name:</span> {name}</h2>
                             <p><span className="text-lg font-bold">Band Name: </span>{brand}</p>
                             <p><span className="text-lg font-bold">Type: </span>{type}</p>
                             <p><span className="text-lg font-bold">Price: </span>$ {price}</p>
-                            <p><span className="text-lg font-bold">Rating: </span>{'rating'}</p>
                         </div>
                         <div className="card-actions justify-end">
                             <button onClick={() => handleDelete(name)} className="btn bg-[#C1032F] text-white hover:bg-[#303030] hover:text-white normal-case">Delete
